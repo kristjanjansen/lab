aa3c5efc54fc26a4919feb75f712a0824f2990b1
kristjanjansen <kristjan.jansen@gmail.com>
2017-06-19 06:36:19 +0300

    removing token for now

--- examples/standalone.js
+++ examples/standalone.js
@@ -1,2 +1,3 @@
 console.log('Hello from standalone experiment')
 console.log(JSON.stringify({here: 'bla'}))
+console.log(process.argv)
--- examples/standalone.py
+++ examples/standalone.py
@@ -1,4 +1,7 @@
 import json
+import sys
 
 print "Hello from test.py"
+print sys.argv[1]
+
 print json.dumps({'some': 'json'})
