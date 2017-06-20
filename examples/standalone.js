020939f28b851dff924f6550850fdabdc413b3ed
kristjanjansen <kristjan.jansen@gmail.com>
2017-06-20 11:34:23 +0300

    fixing examples

--- examples/standalone.js
+++ examples/standalone.js
@@ -1,3 +1,12 @@
 console.log('Hello from standalone experiment')
 console.log(process.argv.slice(2).join(' '))
-console.log(JSON.stringify({some: 'json'}))
+
+var count = 0
+
+var generator = setInterval(() => {
+    console.log(JSON.stringify({value: Math.random()}))
+    count++
+    if (count > 10) {
+        clearInterval(generator)
+    }
+}, 1000)
--- examples/standalone.py
+++ examples/standalone.py
@@ -3,9 +3,13 @@ import sys
 
 print "Hello from test.py"
 
-print sys.argv[1]
-print sys.argv[2]
-print sys.argv[3]
-print sys.argv[4]
+if sys.argv[1:]:
+    print sys.argv[1]
+if sys.argv[2:]:
+    print sys.argv[2]
+if sys.argv[3:]:
+    print sys.argv[3]
+if sys.argv[4:]:
+    print sys.argv[4]
 
 print json.dumps({'some': 'json'})
