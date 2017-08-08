9177c05b2147d32243044baea92c1a74a98e9207
kristjanjansen <kristjan.jansen@gmail.com>
2017-08-08 14:44:26 +0300

    conf to Store

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 const store = require('./lib/utils/Store')
 
-//store.setRun('a1', { hello: 'world'})
-//store.setRun('a2', { hello: 'world2'})
-var a = store.getRun('a2')
+store.setRun('a1', { hello: 'world2'})
+store.setRun('a2', { hello: 'world2'})
+var a = store.getRuns()
 console.log(a)
 
-//store.setConfig('a1', 'aa')
-//store.setConfig('a2', { hello: 'world2'})
+store.setConfig('a1', 'aa')
+store.setConfig('a2', { hello: 'world2'})
 
 var a = store.getConfig('a2')
 console.log(a)
